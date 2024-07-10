fb19ae1e73afc0ac1a6cd67e357d448463b6f281
Recio, Toni <Toni.Recio@travelport.com>
2024-07-10 11:44:38 +0200

    Linter has been setup, using Standard

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,12 @@
 export const metadata = {
-  title: "Minesweeper",
-  description: "The classic Minesweeper game made in React.Js",
-};
+  title: 'Minesweeper',
+  description: 'The classic Minesweeper game made in React.Js'
+}
 
-export default function RootLayout({ children }) {
+export default function RootLayout ({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>{children}</body>
     </html>
-  );
+  )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
-
-export default function Home() {
+export default function Home () {
   return (
     <p>Hello React!</p>
-  );
+  )
 }
