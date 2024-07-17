49ae0e52c2d3c17f254621a04af710c31ab55eec
Recio, Toni <mail@tonirecio.com>
2024-07-17 12:44:37 +0200

    FEAT: Starting game - All the cells should be covered

--- src/components/cell.jsx
+++ src/components/cell.jsx
@@ -1,5 +1,5 @@
 export default function Cell () {
   return (
-    <button className='minefield-cell' data-testid='minefield-cell'>.</button>
+    <button className='minefield-cell covered' data-testid='minefield-cell'>.</button>
   )
 }
