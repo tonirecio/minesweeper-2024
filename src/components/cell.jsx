4a75768c16ab1220ddb371fdd240ecb947e9e366
Recio, Toni <Toni.Recio@travelport.com>
2024-07-19 12:19:10 +0200

    FEAT: Suspecting that a cell is hiding a mine, mouse right click, tagging a cell as mined

--- src/components/cell.jsx
+++ src/components/cell.jsx
@@ -3,18 +3,24 @@ import './styles/cell.css'
 
 export default function Cell ( { rowPosition, colPosition, hasMine, numberOfMinesAround } ) {
   const [isCovered, setIsCovered] = useState(true)
+  const [isTagged, setIsTagged] = useState("")
   function handleClick (e) {
     e.preventDefault()
     setIsCovered(false)
+  }
+  function handleContextMenu (e) {
+    e.preventDefault()
+    setIsTagged('mined')
   }
 
   if (isCovered) {
     return (
       <button
         onClick={handleClick}
+        onContextMenu={handleContextMenu}
         data-testid={`minefield-cell cell-row${rowPosition}-col${colPosition}`}
         className='minefield-cell covered'
-      />
+      >{ isTagged === 'mined' && <img src='/tiles/flagCell.png'/>}</button>
     )
   } else {
     return (
