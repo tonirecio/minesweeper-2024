e14268df6c0d334307049685b8b9d32a2035f5d5
Anastasiia Levkina <anastasiia.levkina.7e5@itb.cat>
2024-07-26 12:43:51 +0200

    TEST: Starting game, default game status is waiting, the button status show a happy face

--- tests/minesweeper.web.test.js
+++ tests/minesweeper.web.test.js
@@ -3,5 +3,12 @@ import * as steps from './steps/minesweeper.web.steps'
 
 const feature = loadFeature('./tests/features/minesweeper.web.feature')
 defineFeature(feature, (test) => {
-
+  test('Starting game, default game status is waiting, the button status show a happy face', ({ given, then, pending }) => {
+    given('the player opens the game', () => {
+      steps.openTheGame()
+    })
+    then('the button status should show a happy face', () => {
+      pending()
+    })
+  })
 })
