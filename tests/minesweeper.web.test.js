1ac3441aaa8496982f20044374c4469ac54a3e95
Anastasiia Levkina <anastasiia.levkina.7e5@itb.cat>
2024-07-26 13:11:46 +0200

    TEST: Change tag from mined to inconclusive, the remaining mines counter increase

--- tests/minesweeper.web.test.js
+++ tests/minesweeper.web.test.js
@@ -192,4 +192,21 @@ defineFeature(feature, (test) => {
       pending()
     })
   })
+  test('Change tag from mined to inconclusive, the remaining mines counter increase', ({ given, and, when, then, pending }) => {
+    given('the player opens the game', () => {
+      pending()
+    })
+    given('the player loads the following mock data', (docString) => {
+      pending()
+    })
+    and(/^the player tags as mined the cell \("(.*)","(.*)"\)$/, (arg0, arg1) => {
+      pending()
+    })
+    when(/^the player tags as inconclusive the cell \("(.*)","(.*)"\)$/, (arg0, arg1) => {
+      pending()
+    })
+    then(/^the remaining mines counter should be "(.*)"$/, (arg0) => {
+      pending()
+    })
+  })
 })
