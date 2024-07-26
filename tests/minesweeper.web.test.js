ffe5950aa34a530f8f658b061ac9d8c44145a712
Max Dron <maxdron@icloud.com>
2024-07-26 12:59:30 +0200

    TEST: Starting game, default game status is waiting, the button status show a happy face

--- tests/minesweeper.web.test.js
+++ tests/minesweeper.web.test.js
@@ -4,13 +4,13 @@ import * as steps from './steps/minesweeper.web.steps'
 const feature = loadFeature('./tests/features/minesweeper.web.feature')
 
 defineFeature(feature, test => {
-  test('Starting game, default game status is waiting, the button status show a happy face', ({ given, then, pending }) => {
+  test('Starting game, default game status is waiting, the button status show a happy face', ({ given, then }) => {
     given('the player opens the game', () => {
-
+      steps.openTheGame()
     })
 
     then('the button status should show a happy face', () => {
-      pending()
+      expect(steps.isStatusButtonShowing('happy face')).toBe(true)
     })
   })
 
--- tests/steps/minesweeper.web.steps.js
+++ tests/steps/minesweeper.web.steps.js
@@ -1,2 +1,12 @@
 import { render, screen, fireEvent } from '@testing-library/react'
 import Game from '../../src/components/game'
+
+export function openTheGame () {
+  render(<Game />)
+}
+// TODO refactor to use alt instead of src
+export function isStatusButtonShowing (status) {
+  const faceButton = screen.getByTestId('face-button', { exact: true })
+  const imgSource = faceButton.getElementsByTagName('img')[0].src
+  return imgSource.includes(status + '.png')
+}
