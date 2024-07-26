2a107d38b18e30b3b070eed708324cc4fb0a72a5
Anastasiia Levkina <anastasiia.levkina.7e5@itb.cat>
2024-07-26 13:08:13 +0200

    TEST: Untagging a cell as mine, the remaining mines counter increase

--- tests/minesweeper.web.test.js
+++ tests/minesweeper.web.test.js
@@ -144,4 +144,21 @@ defineFeature(feature, (test) => {
       pending()
     })
   })
+  test('Untagging a cell as mine, the remaining mines counter increase', ({ given, and, when, then, pending }) => {
+    given('the player opens the game', () => {
+      pending()
+    })
+    given('the player loads the following mock data', (docString) => {
+      pending()
+    })
+    and(/^the player tags as mined the cell \("(.*)","(.*)"\)$/, (arg0, arg1) => {
+      pending()
+    })
+    when(/^the player untags the cell \("(.*)","(.*)"\)$/, (arg0, arg1) => {
+      pending()
+    })
+    then(/^the remaining mines counter should be "(.*)"$/, (arg0) => {
+      pending()
+    })
+  })
 })
