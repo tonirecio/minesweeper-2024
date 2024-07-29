cb397ccab4381db34523f2620ea0065df2a411a9
Max Dron <maxdron@icloud.com>
2024-07-29 09:49:43 +0200

    PROJECT: Style Fix

--- src/components/statusImg.jsx
+++ src/components/statusImg.jsx
@@ -2,6 +2,6 @@ import '@/components/styles/statusImg.css'
 
 export default function StatusImg () {
   return (
-    <img src='/faces/happy-face.png' alt='face-smile' data-testid='face-img' />
+    <img src='/faces/happy-face.png' alt='face-smile' data-testid='face-img' className='status-img' />
   )
 }
