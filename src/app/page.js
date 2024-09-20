a18510d642f43594bd1194f965f21b9775fbd5f3
Toni Recio <6155833+tonirecio@users.noreply.github.com>
2024-09-20 08:28:44 +0000

    REFACTOR: Removing unnecessary code at Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,7 @@
 import Game from '../components/Game'
 import StoreProvider from '../components/StoreProvider'
-import { makeStore } from '../lib/store'
-import { useRef } from 'react'
 
 export default function Home () {
-  const storeRef = useRef()
-  if (!storeRef.current) {
-    storeRef.current = makeStore()
-  }
-
   return (
     <StoreProvider>
       <Game />
