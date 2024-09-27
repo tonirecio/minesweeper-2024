56586da6bbe0cfe5104ad124a97eeae0f7607723
Toni Recio <6155833+tonirecio@users.noreply.github.com>
2024-09-27 10:55:32 +0000

    REFACTOR: MockDataForm JS->TS

--- src/components/MockDataForm.tsx
+++ src/components/MockDataForm.tsx
@@ -1,23 +1,26 @@
 import { useState } from "react";
 
-export default function MockDataForm({ setData }) {
+interface MockDataFormProps {
+  setData: (data: string) => void;
+}
+
+export default function MockDataForm({ setData }: MockDataFormProps) {
   const [mockData, setMockData] = useState("| * | o |");
-  function handleSubmit(e) {
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     setData(mockData);
   }
 
-  const handleInputChange = (e) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setMockData(e.target.value);
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <textarea
-        type="text"
         value={mockData}
-        cols="40"
-        rows="8"
+        cols={40}
+        rows={8}
         onChange={handleInputChange}
         data-testid="mock-data-input"
         style={{ width: "100%", height: "300px" }}
