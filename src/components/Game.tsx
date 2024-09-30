b2c97813141843008a14c98651d3b599c20ee251
Toni Recio <6155833+tonirecio@users.noreply.github.com>
2024-09-30 15:40:54 +0000

    REFACTOR: Deepsource, useCallBack

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,15 +1,15 @@
-"use client"; // TODO Why do we need this here?
-import { useState, useEffect, ReactElement } from "react";
+"use client";
+import { useState, useEffect, useCallback, ReactElement } from "react";
 import Minefield from "./Minefield";
 import MockDataForm from "./MockDataForm";
 
 /**
  * The `Game` component represents the main game interface for Minesweeper.
  * It manages the state for mock data visibility and mock data content.
- * 
+ *
  * - Toggles the visibility of the mock data form when the user presses `Ctrl + M`.
  * - Updates the mock data and hides the form when new data is set.
- * 
+ *
  * @component
  * @returns {ReactElement} The rendered game interface.
  */
@@ -30,14 +30,14 @@ export default function Game(): ReactElement {
    *
    * @param data - The new mock data to be set.
    */
-  function setMockDataForm(data: string): void {
+  const setMockDataForm = useCallback((data: string): void => {
     setMockData(data);
     setMockDataFormVisible(false);
-  }
+  }, []);
 
   /**
    * Handles the key press event to toggle the visibility of the mock data form.
-   * 
+   *
    * @param {KeyboardEvent} e - The keyboard event object.
    * @returns {void}
    */
